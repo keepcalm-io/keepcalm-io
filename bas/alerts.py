5d8fa820a337bad44174d2d8058616d8b1128454
Diego Navarro Mellen <dnmellen@gmail.com>
2014-01-18 16:29:58 +0100

    Alert system listens for redis expired keys

--- bas/alerts.py
+++ bas/alerts.py
@@ -9,7 +9,7 @@ import redis
 # Config logger
 try:
     LOGFILE = os.environ['KEEPCALM_ALERTS_LOG']
-except IndexError:
+except KeyError:
     LOGFILE = '/tmp/keepcalm_alerts.log'
 
 logger = logging.getLogger('keepcalm')
@@ -17,7 +17,16 @@ handler = logging.FileHandler(LOGFILE)
 formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
 handler.setFormatter(formatter)
 logger.setLevel(logging.DEBUG)
+
+def handle_alert(key):
+    """
+    Handler to alert user for an expired task
+    """
+
+    # TODO: handle if key expired is valid 
+    print("Expired {}".format(key))
  
+
 @asyncio.coroutine
 def listener(redis_conn, channels):
  
@@ -25,11 +34,17 @@ def listener(redis_conn, channels):
     pubsub.subscribe(channels)
     logger.debug('Listening redis...')
     for item in pubsub.listen():
-        print(item)
+        try:
+            expired_key = item['data']
+            if isinstance(expired_key, bytes):
+                handle_alert(expired_key.decode('utf-8'))
+                logger.debug(item)
+        except KeyError:
+            continue
  
  
 if __name__ == '__main__':
     r = redis.Redis()
     loop = asyncio.get_event_loop()
-    asyncio.Task(listener(r, ['test']))
+    asyncio.Task(listener(r, ['__keyevent@0__:expired']))
     loop.run_forever()
